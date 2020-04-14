20c5204dbcc7a2767f304c17f0ab8ff9afb7e2ee
Zoki Krstovski <zoran.krstovski@alosys.it>
2020-04-14 22:54:20 +0200

    Responsive Task 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ResponsiveScreenComponent} from './responsive-screen/responsive-screen.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'responsive-screen', component: ResponsiveScreenComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ResponsiveScreenComponent } from './responsive-screen/responsive-screen.component';
+import {DeviceDetectorService} from 'ngx-device-detector';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResponsiveScreenComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    DeviceDetectorService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
